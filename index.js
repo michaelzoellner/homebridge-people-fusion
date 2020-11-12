9e93c764f1ceaa51b12f512ed8d4071021d87dd9
michaelzoellner <michael.zoellner@rwth-aachen.de>
2020-11-12 16:58:26 +0100

    services

--- index.js
+++ index.js
@@ -465,6 +465,59 @@ function PeopleAllAccessory(log, name, platform) {
     this.platform = platform;
     this.state = 0;
 
+    class LastActivationCharacteristic extends Characteristic {
+        constructor(accessory) {
+            super('LastActivation', 'E863F11A-079E-48FF-8F27-9C2605A29F52');
+            this.setProps({
+                format: Characteristic.Formats.UINT32,
+                unit: Characteristic.Units.SECONDS,
+                perms: [
+                    Characteristic.Perms.READ,
+                    Characteristic.Perms.NOTIFY
+                ]
+            });
+        }
+    }
+
+    class SensitivityCharacteristic extends Characteristic {
+        constructor(accessory) {
+            super('Sensitivity', 'E863F120-079E-48FF-8F27-9C2605A29F52');
+            this.setProps({
+                format: Characteristic.Formats.UINT8,
+                minValue: 0,
+                maxValue: 7,
+                validValues: [0, 4, 7],
+                perms: [
+                    Characteristic.Perms.READ,
+                    Characteristic.Perms.NOTIFY,
+                    Characteristic.Perms.WRITE
+                ]
+            });
+        }
+    }
+
+    class DurationCharacteristic extends Characteristic {
+        constructor(accessory) {
+            super('Duration', 'E863F12D-079E-48FF-8F27-9C2605A29F52');
+            this.setProps({
+                format: Characteristic.Formats.UINT16,
+                unit: Characteristic.Units.SECONDS,
+                minValue: 5,
+                maxValue: 15 * 3600,
+                validValues: [
+                    5, 10, 20, 30,
+                    1 * 60, 2 * 60, 3 * 60, 5 * 60, 10 * 60, 20 * 60, 30 * 60,
+                    1 * 3600, 2 * 3600, 3 * 3600, 5 * 3600, 10 * 3600, 12 * 3600, 15 * 3600
+                ],
+                perms: [
+                    Characteristic.Perms.READ,
+                    Characteristic.Perms.NOTIFY,
+                    Characteristic.Perms.WRITE
+                ]
+            });
+        }
+    }
+
     this.service = new Service.OccupancySensor(this.name);
     this.service
         .getCharacteristic(Characteristic.OccupancyDetected)
