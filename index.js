3f18e32b4482f0f04be61c8c2d448ccd201c9485
michaelzoellner <michael.zoellner@rwth-aachen.de>
2020-11-14 22:00:02 +0100

    changes?

--- index.js
+++ index.js
@@ -1159,7 +1159,7 @@ MotionSensorAccessory.prototype.setDuration = function(value) {
 
 MotionSensorAccessory.prototype.setSensitivity = function(value) {
   this.log('setSensitivity triggered with value of %s', value);
-  this.platform.storage.setItemSync('sensitivity_' + this.name, value + 1); // in storage, the value +1 will be stored 
+  this.platform.storage.setItemSync('sensitivity_' + this.name, value + 1); // in storage, the value +1 will be stored
   this.sensitivity = value;
 }
 
